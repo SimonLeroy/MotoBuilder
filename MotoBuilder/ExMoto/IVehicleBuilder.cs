3dab33e071326605c95686bb086efba75989ee64
Simon Leroy <s.leroy@proxiad.rouen>
2018-11-09 13:59:11 +0100

    Ajoutez des fichiers projet.

---
+++ MotoBuilder/ExMoto/IVehicleBuilder.cs
@@ -0,0 +1,20 @@
+﻿using MotoBuilder.ExMoto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoBuilder
+{
+    public interface IVehicleBuilder
+    {
+        void SetModel();
+        void SetEngine();
+        void SetTransmission();
+        void SetBody();
+        void SetAccessories();
+
+        Vehicle GetVehicle();
+    }
+}
---
+++ MotoBuilder/ExMoto/Prog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoBuilder.ExMoto
+{
+    class Prog
+    {
+ //       var vehicleCreator = new VehicleCreator(new HeroBuilder());
+ //       vehicleCreator.CreateVehicle();
+ //var vehicle = vehicleCreator.GetVehicle();
+ //       vehicle.ShowInfo();
+ 
+ //Console.WriteLine("---------------------------------------------");
+ 
+ //vehicleCreator = new VehicleCreator(new HondaBuilder());
+ //vehicleCreator.CreateVehicle();
+ //vehicle = vehicleCreator.GetVehicle();
+ //vehicle.ShowInfo();
+ 
+ //Console.ReadKey();
+    }
+}
